5fc69a0c95480e244e4249a264f46012191cfeff
mahi7000 <mahlet7000@gmail.com>
2024-03-25 04:38:05 -0700

    1-binary_tree_insert_left.c

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,31 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_left - inserts node as left child
+ * @parent: parent of left child
+ * @value: value to store in new node
+ *
+ * Return: NULL or pointer to new node
+ */
+
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
+
+	if (new_node == NULL || parent == NULL)
+		return (NULL);
+
+	new_node->n = value;
+	new_node->parent = parent;
+	new_node->left = NULL;
+	new_node->right = NULL;
+
+	if (parent->left != NULL)
+	{
+		new_node->left = parent->left;
+		parent->left->parent = new_node;
+	}
+	parent->left = new_node;
+
+	return (new_node);
+}
