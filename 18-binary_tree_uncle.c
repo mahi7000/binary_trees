ae5f4199067972721a91fab704eb55f3a28fddb0
mahi7000 <mahlet7000@gmail.com>
2024-03-26 03:02:53 -0700

    18-binary_tree_uncle.c

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -9,15 +9,11 @@
 
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	binary_tree_t *parent;
-
-	parent = node->parent;
-
-	if (node == NULL || parent == NULL || parent->parent == NULL)
+	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
 		return (NULL);
 
-	if (parent->parent->left == parent)
-		return (parent->parent->right);
+	if (node->parent->parent->left == node->parent)
+		return (node->parent->parent->right);
 	else
-		return (parent->parent->left);
+		return (node->parent->parent->left);
 }
