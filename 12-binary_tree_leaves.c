7ba2b3bb18c3757c00bb31a6c1b0a2dd13ad7724
mahi7000 <mahlet7000@gmail.com>
2024-03-26 01:20:37 -0700

    Check leaf

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -9,13 +9,15 @@
 
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
+	size_t left_leaf, right_leaf;
+
 	if (tree == NULL)
 		return (0);
 	if (tree->left == NULL && tree->right == NULL)
 		return (1);
 
-	size_t left_leaf = binary_tree_leaves(tree->left);
-	size_t right_leaf = binary_tree_leaves(tree->right);
+	left_leaf = binary_tree_leaves(tree->left);
+	right_leaf = binary_tree_leaves(tree->right);
 
 	return (left_leaf + right_leaf);
 }
