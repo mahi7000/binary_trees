f9f1da68cc1a6f2f1ac960027103f7bb0f73810b
mahi7000 <mahlet7000@gmail.com>
2024-03-26 01:16:32 -0700

    Check height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,12 +9,13 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	size_t left_height, right_height, height;
+
 	if (tree == NULL)
 		return (0);
 
-	size_t left_height = binary_tree_height(tree->left);
-	size_t right_height = binary_tree_height(tree->right);
-	size_t height;
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
 
 	if (tree->left == NULL && tree->right == NULL)
 		return (0);
