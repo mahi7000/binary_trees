0c1cb52fcad61196163170e21a2585fdfc23e29a
mahi7000 <mahlet7000@gmail.com>
2024-03-26 01:17:42 -0700

    Check depth

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -9,10 +9,12 @@
 
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
+	size_t height;
+
 	if (tree == NULL || tree->parent == NULL)
 		return (0);
 
-	size_t height = binary_tree_depth(tree->parent);
+	height = binary_tree_depth(tree->parent);
 
 	return (height + 1);
 }
